e6647664e553041d007d459f8dd50cf17a0579b2
Your Name <you@example.com>
2021-03-14 15:33:21 -0400

    First Commit

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default class App extends Component {
+
+  constructor() {
+    super();
+    this.pageCount = [1, 2, 3]  
+  }
+
+  render() {
+    var pages = this.pageCount[this.pageCount.length - 1];
+      return(
+        <View style={styles.container}>
+          <TouchableOpacity onPress={() => {this.pageCount.push(pages + 1); pages=this.pageCount[this.pageCount.length - 1]; console.log(this.pageCount)}}><Text>Add Text</Text></TouchableOpacity>
+          <View>
+            {this.pageCount.map((item, key) => {
+              return <Text key={key}>{item}</Text>
+            })}
+          </View>
+        </View>
+      )
+  }
+}   
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +31,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
---
+++ components/Card.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+
+class Card extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>MyClass</Text>
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2c3e50',
+    },
+});
+
+export default Card;
